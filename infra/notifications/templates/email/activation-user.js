5e74e62b2c7dac47989fdea1f01d62b53f61855e
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2026-01-25 00:00:24 -0300

    feat: update user activation email template

--- infra/notifications/templates/email/activation-user.js
+++ infra/notifications/templates/email/activation-user.js
@@ -1,9 +1,14 @@
 import webserver from "infra/webserver";
+import activation from "models/activation";
 
 export function activationUserTemplateEmail(user, activationToken) {
-  return `${user.username}, clique no link abaixo para ativar seu cadastro no Gwalchmei.
-
+  return `Olá ${user.username}, seja bem-vindo(a)!
+  
+Clique no link abaixo para ativar seu cadastro no LinkRental.
 ${webserver.origin}/cadastro/ativar/${activationToken.id}
+
+O código expira em ${activation.EXPIRATION_IN_MILLISECONDS / 60000} minutos.
+
 
 Atenciosamente,
 Equipe Gwalchmei`;
